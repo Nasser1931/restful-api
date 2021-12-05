86cd33c54fe6c9ab2f8339d4b4c7a00a24d2d329
Nasser1931 <74050604+Nasser1931@users.noreply.github.com>
2021-12-05 12:52:21 +0400

    added comments

--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -5,11 +5,11 @@ module.exports = app => {
 
     router.post("/", blogs.create);
 
-    // Retrieve all Tutorials
+    // Retrieve all blogs
     router.get("/", blogs.findAll);
     router.put("/:id", blogs.update);
 
-    // Delete a Tutorial with id
+    // Delete a Blog with id
     router.delete("/:id", blogs.delete);
     app.use('/blog', router);
 }
